4b341c9d6593b4553ed662fa1035d645226f9108
Johan Tenghamn <tenghamn@chalmers.se>
2019-09-26 10:51:55 +0200

    Added setup.py and requirements

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='1d-particle-tracking',
+      version='0.0.1',
+      description='Particle tracking in Kymographs',
+      long_description=long_description,
+      url='',
+      author='Johan Tenghamn',
+      author_email='tenghamn@chalmers.se',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=requirements,
+      keywords='particle tracking nanochannel',
+      classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3.6', 'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering :: Physics'],
+      zip_safe=False)
