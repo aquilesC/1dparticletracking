7f50d083dad98ae354a888c00b16fb44608b262d
Johan Tenghamn <tenghamn@chalmers.se>
2019-09-30 12:47:02 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='particle_tracker_one_d',
-      version='0.0.5',
+      version='0.0.6',
       description='Particle tracking in Kymographs',
       long_description=long_description,
       long_description_content_type="text/markdown",
