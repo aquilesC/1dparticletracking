7c78ba4b607819cb37192f1936765ccd5d991bd5
Johan Tenghamn <tenghamn@chalmers.se>
2019-10-17 16:02:27 +0200

    Startded with documentation

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+version = '0.1.5'
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+if __name__ == "__main__":
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
 
-setup(name='particle_tracker_one_d',
-      version='0.1.5',
-      description='Particle tracking in Kymographs',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='',
-      author='Johan Tenghamn',
-      author_email='tenghamn@chalmers.se',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=requirements,
-      keywords='particle tracking nanochannel',
-      classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3.6', 'Intended Audience :: Science/Research',
-                   'Topic :: Scientific/Engineering :: Physics'],
-      zip_safe=False)
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+    setup(name='particle_tracker_one_d',
+          version=version,
+          description='Particle tracking in Kymographs',
+          long_description=long_description,
+          long_description_content_type="text/markdown",
+          url='',
+          author='Johan Tenghamn',
+          author_email='tenghamn@chalmers.se',
+          license='MIT',
+          packages=find_packages(),
+          install_requires=requirements,
+          keywords='particle tracking nanochannel',
+          classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3.6', 'Intended Audience :: Science/Research',
+                       'Topic :: Scientific/Engineering :: Physics'],
+          zip_safe=False)
