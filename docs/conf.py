fe06469f64162e73af22da21e556fe34d7607cf0
Johan Tenghamn <tenghamn@chalmers.se>
2019-10-18 16:03:42 +0200

    Trying to get readthedocs to work

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,7 @@ extensions = [
 ]
 
 # Set the master doc
+source_suffix = '.rst'
 master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
@@ -50,6 +51,8 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
