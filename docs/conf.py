aba5c8c34c113dccf953b3a1d049f82896088e0d
Johan Tenghamn <tenghamn@chalmers.se>
2019-10-18 13:02:12 +0200

    Working on documentation

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,10 @@
 #
 import os
 import sys
+import sphinx_theme
+
 sys.path.insert(0, os.path.abspath('..'))
 from setup import version
-
 
 # -- Project information -----------------------------------------------------
 
@@ -25,13 +26,18 @@ author = 'Johan Tenghamn'
 # The full version, including alpha/beta/rc tags
 release = version
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -42,15 +48,21 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+html_theme = 'neo_rtd_theme'
+html_theme_path = [sphinx_theme.get_html_theme_path()]
+
+html_logo = '_static/logo.png'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
