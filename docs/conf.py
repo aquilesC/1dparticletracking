db504148d85ec256f03e6c3494f00bb678844da9
Johan Tenghamn <tenghamn@chalmers.se>
2019-10-18 15:45:34 +0200

    Set the index.rst as master in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,9 @@ extensions = [
     'numpydoc',
 ]
 
+# Set the master doc
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
