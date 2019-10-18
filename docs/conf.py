9ab0a0797698a10bc0821145eafd61bf1b0a68ed
Johan Tenghamn <tenghamn@chalmers.se>
2019-10-18 16:11:06 +0200

    Testing for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -60,8 +60,8 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 
-html_theme = 'neo_rtd_theme'
-html_theme_path = [sphinx_theme.get_html_theme_path()]
+#html_theme = 'neo_rtd_theme'
+#html_theme_path = [sphinx_theme.get_html_theme_path()]
 
 html_logo = '_static/logo.png'
 
