ff8dc27335d4d4cfe95350032504a2a3f6cde103
Johan Tenghamn <tenghamn@chalmers.se>
2020-05-11 14:40:30 +0200

    Fixed test error

--- tests/unit/test_trajectory.py
+++ tests/unit/test_trajectory.py
@@ -11,9 +11,9 @@ class CalculateDiffusionCoefficientTester(unittest.TestCase):
         """
 
         # Simplest test, diffusion coefficient = 0
-        expected_diffusion_coefficient = 0
+        expected_diffusion_coefficient = 0.0
 
-        particle_positions = np.empty((5,), dtype=[('frame_index', np.int16), ('time', np.float32), ('position', np.float32)])
+        particle_positions = np.empty((3,), dtype=[('frame_index', np.int16), ('time', np.float32), ('position', np.float32)])
 
         particle_positions['frame_index'] = [0, 1, 2]
         particle_positions['time'] = [0, 1, 2]
@@ -26,7 +26,7 @@ class CalculateDiffusionCoefficientTester(unittest.TestCase):
         t = Trajectory(pixel_width=pixel_width)
 
         t._particle_positions = particle_positions
-        calculated_diffusion_coefficient = t.calculate_diffusion_coefficient_using_covariance_based_estimator(R=1/4)
+        calculated_diffusion_coefficient, error = t.calculate_diffusion_coefficient_using_covariance_based_estimator(R=R)
 
         self.assertEqual(expected_diffusion_coefficient, calculated_diffusion_coefficient)
 
